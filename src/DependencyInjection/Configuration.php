f339541ea7c2ea89ccb9d82b76f921e2723639d7
Peter Gribanov <wcode404@gmail.com>
2020-01-21 19:25:45 +0300

    remove not used NodeDefinition in Configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,7 +9,6 @@
 
 namespace GpsLab\Bundle\GeoIP2Bundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
