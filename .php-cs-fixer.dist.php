55c04842ff97edf2951854731e431ba8ad74170a
Peter Gribanov <wcode404@gmail.com>
2021-07-12 17:49:29 +0300

    update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,31 +9,32 @@ GpsLab component.
 @license   http://opensource.org/licenses/MIT
 EOF;
 
-return PhpCsFixer\Config::create()
-    ->setRules([
-        '@Symfony' => true,
-        'array_syntax' => [
-            'syntax' => 'short',
-        ],
-        'header_comment' => [
-            'commentType' => 'PHPDoc',
-            'header' => $header,
-        ],
-        'class_definition' => [
-            'multiLineExtendsEachSingleLine' => true,
-        ],
-        'no_superfluous_phpdoc_tags' => false,
-        'blank_line_after_opening_tag' => false,
-        'phpdoc_no_empty_return' => false,
-        'yoda_style' => false,
-        'ordered_imports' => [
-            'sort_algorithm' => 'alpha',
-        ],
-    ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->in(__DIR__.'/src')
-            ->in(__DIR__.'/tests')
-            ->notPath('bootstrap.php')
-    )
+$rules = [
+    '@Symfony' => true,
+    'array_syntax' => [
+        'syntax' => 'short',
+    ],
+    'header_comment' => [
+        'comment_type' => 'PHPDoc',
+        'header' => $header,
+    ],
+    'no_superfluous_phpdoc_tags' => false,
+    'blank_line_after_opening_tag' => false,
+    'phpdoc_no_empty_return' => false,
+    'yoda_style' => false,
+    'ordered_imports' => [
+        'sort_algorithm' => 'alpha',
+    ],
+];
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+    ->notPath('bootstrap.php')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules($rules)
+    ->setFinder($finder)
+    ->setUsingCache(true)
 ;
