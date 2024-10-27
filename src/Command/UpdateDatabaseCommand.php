b8397f8cafc2bac4bb1f3643974dcd3bcbd7a540
Ante de Baas <ante@debaas.net>
2024-10-27 02:46:01 +0100

    do not download when licence is not set

--- src/Command/UpdateDatabaseCommand.php
+++ src/Command/UpdateDatabaseCommand.php
@@ -115,7 +115,11 @@ EOT;
 
             $io->section(sprintf('Update "%s" database', $database));
 
-            $this->downloader->download($this->databases[$database]['url'], $this->databases[$database]['path']);
+            if (empty($this->databases[$database]['license'])) {
+                $io->warning(sprintf('`GeoIP Database %s has no maxmind license key', $database));
+            } else {
+                $this->downloader->download($this->databases[$database]['url'], $this->databases[$database]['path']);
+            }
 
             $io->comment(sprintf('Database <info>%s</info> updated', $database));
         }
